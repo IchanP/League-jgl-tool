e0b7352b3cc8506deb8d2f3de76264bbd1c3cab6
Pontus Grandin <pg222pb@student.lnu.se>
2023-04-14 06:24:14 +0200

    File structure

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    ['@babel/preset-react', {runtime: 'automatic'}],
-  ],
-};
+	presets: [
+		'@babel/preset-env',
+		['@babel/preset-react', {runtime: 'automatic'}],
+	],
+}
--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    moduleNameMapper: {
-      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+	testEnvironment: 'jest-environment-jsdom',
+	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
         '<rootDir>/__mocks__/fileMock.js',
-      '\\.(css|less)$': 'identity-obj-proxy',
-    },
-  };
+		'\\.(css|less)$': 'identity-obj-proxy',
+	},
+}
   
