009b93d4e0bea1201df2bf0541d427a189671b1f
Pontus Grandin <pg222pb@student.lnu.se>
2023-04-14 06:59:19 +0200

    Add map map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import React from 'react'
 import Footer from './Components/Footer/Footer'
+import Map from './Components/Map/Map'
 
 function App() {
 
 	return (
 		<div className="App">
 			<h1 className="mainTitle">Jingle Gap</h1>
+			<Map />
 			<Footer />
 		</div>
 	)
---
+++ src/Components/Map/Map.jsx
@@ -0,0 +1,12 @@
+import './map.css'
+import React from 'react'
+
+const Map = () => {
+	return ( 
+		<div className="mapwrap">
+			<p>Hello</p>
+		</div>
+	)
+}
+ 
+export default Map
