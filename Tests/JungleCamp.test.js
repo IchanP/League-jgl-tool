76296dff4b5643dcd2eaaf1f11e1e4cf3b03444a
Pontus Grandin <pg222pb@student.lnu.se>
2023-04-14 08:46:43 +0200

    Add images to camps, write 2 basic tests for jungle camp

---
+++ Tests/JungleCamp.test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { render, fireEvent, waitFor, act } from '@testing-library/react'
+import Map from '../src/Components/Map/Map'
+import '@testing-library/jest-dom'
+
+describe('JungleCamp component', () => {
+	it('should set campSelected to true when button with id "gromp-blue" is clicked once', async () => {
+		render(<Map />)
+		const grompBlueButton = document.getElementById('gromp-blue')
+
+		act(() => {
+			fireEvent.click(grompBlueButton)
+		})
+
+		await waitFor(() => {
+			expect(grompBlueButton).toBeTruthy()
+		})
+
+		expect(grompBlueButton).toHaveAttribute('data-iscampselected', 'true')
+
+	})
+})
+
+describe('JungleCamp component', () => {
+	it('should set campSelected to false when button with id "gromp-blue" is clicked twice', async () => {
+		render(<Map />)
+		const grompBlueButton = document.getElementById('gromp-blue')
+
+		act(() => {
+			fireEvent.click(grompBlueButton)
+		})
+
+		await waitFor(() => {
+			expect(grompBlueButton).toBeTruthy()
+		})
+
+		expect(grompBlueButton).toHaveAttribute('data-iscampselected', 'true')
+
+		act(() => {
+			fireEvent.click(grompBlueButton)
+		})
+
+		expect(grompBlueButton).toHaveAttribute('data-iscampselected', 'false')
+	})
+})
--- src/Components/JungleCamp/JungleCamp.jsx
+++ src/Components/JungleCamp/JungleCamp.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './junglecamp.css'
 
 const JungleCamp = ({ theCamp, goldValue, expValue }) => {
-	console.log(theCamp + goldValue + expValue)
+	
+	const [campSelected, setCampSelected] = useState(false)
+
 	const [campName] = theCamp.split('-')
 	console.log(campName)
 	return ( 
-		<button className="buttonCamp" id={theCamp}>
-			<img></img> 
+		// eslint-disable-next-line react/no-unknown-property
+		<button className="buttonCamp" id={theCamp} data-iscampselected={campSelected} onClick={() => {
+			campSelected === false
+				? setCampSelected(true)
+				: setCampSelected(false)
+		}}>
+			<img src={`./images/${campName}.png`} alt={campName} className="campImage"></img> 
 		</button>
 	)
 }
