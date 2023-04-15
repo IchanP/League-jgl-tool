586e8c5693a66a2a97b533c40d36cf7fe6d97774
Pontus Grandin <pg222pb@student.lnu.se>
2023-04-16 01:09:44 +0200

    Level up and down functionality

--- src/Components/ExpDisplay/ExpDisplay.jsx
+++ src/Components/ExpDisplay/ExpDisplay.jsx
@@ -1,16 +1,9 @@
-import React, { useContext, useState } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import './expdisplay.css'
 import { CampSelectionContext } from '../../Contexts/CampSelectionContext'
 
 const ExpDisplay = () => {
-
-	const totalRequired = [ 280, 380, 480, 580, 680, 780, 880, 980, 1080, 1180, 1280, 1380, 1480, 1580, 1680, 1780, 1880]
-	const [level, setLevel] = useState(1)
-	const [totalExp, setTotalExp] = useState(0)
-
-	const { selectedCamps } = useContext(CampSelectionContext)
-	console.log(selectedCamps)
-	console.log(selectedCamps[0]?.dataset.expvalue)
+	const { selectedCamps, totalExp, level, totalRequired } = useContext(CampSelectionContext)
 	return (  
 		<div className="expDisplay">
 			{selectedCamps.map((camp) => (
--- src/Contexts/CampSelectionContext.jsx
+++ src/Contexts/CampSelectionContext.jsx
@@ -7,24 +7,61 @@ const CampSelectionContextProvider = ({children}) => {
 
 	const [campNumber, setCampNumber] = useState(0)
 	const [selectedCamps, setSelectedCamps] = useState([])
-  
+	const [totalExp, setTotalExp] = useState(0)
+	const [level, setLevel] = useState(1)
+	const totalRequired = [ 280, 380, 480, 580, 680, 780, 880, 980, 1080, 1180, 1280, 1380, 1480, 1580, 1680, 1780, 1880]
+	const expAdd = (expvalue) => {
+		if (selectedCamps.length === 0) {
+			setTotalExp((Number(expvalue) + 150) - totalRequired[level - 1])
+			Number(expvalue) + 150 >= totalRequired[level - 1]
+				? setLevel(level + 1)
+				: {}
+		} else {
+			if(totalExp + Number(expvalue) >= totalRequired[level - 1]) {
+				setTotalExp((totalExp + Number(expvalue)) - totalRequired[level - 1])
+				setLevel(level + 1)
+			} else {
+				setTotalExp(totalExp + Number(expvalue))
+			}
+		}
+	}
+	const expDetract = (expvalue) => {
+		if(selectedCamps.length === 1) {
+			setTotalExp(0)
+			setLevel(level - 1)
+		} else {
+			if(totalExp - Number(expvalue) < 0) {
+				const amountToDetract = totalExp - Number(expvalue)
+				const nextRequiredExp = totalRequired[level - 2]
+				setTotalExp(nextRequiredExp + amountToDetract)
+				setLevel(level - 1)
+			} else {
+				setTotalExp(totalExp - Number(expvalue))
+			}
+		}
+	}
+
 	return <CampSelectionContext.Provider
 		value={{
 			// Add attributes here
 			campNumber: campNumber,
+			totalExp: totalExp,
+			level: level,
+			totalRequired: totalRequired,
 			resetAll: () => {
 				setSelectedCamps([])
 				setCampNumber(0)
 			},
 			addToCampNumber: (e) => {
+				expAdd(e.dataset.expvalue)
 				setCampNumber(campNumber + 1)
 				setSelectedCamps([...selectedCamps, e])
 			},
 			removeFromCampNumber: (e) => {
+				expDetract(e.dataset.expvalue)
 				const updatedSelectedCamps = selectedCamps.filter((element) => element !== e)
 				setSelectedCamps(updatedSelectedCamps)
-				setCampNumber(updatedSelectedCamps.length)
-				return updatedSelectedCamps
+				setCampNumber(updatedSelectedCamps.length)			
 			},
 			selectedCamps: selectedCamps
 		}}
