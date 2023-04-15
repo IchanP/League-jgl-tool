6ed147bf9110ae49d8bdfdc90d460761daa7fa2f
Pontus Grandin <pg222pb@student.lnu.se>
2023-04-15 05:33:32 +0200

    New test for CampSelectionContext

--- Tests/CampSelectionContest.test.js
+++ Tests/CampSelectionContest.test.js
@@ -53,3 +53,36 @@ describe('<CampSelectionProvider />', () => {
 		expect(selectedCampsP).toHaveTextContent('0')
 	})
 })
+
+describe('<CampSelectionProvider />', () => {
+	it('removeFromCampNumber removes all matching elements from selectedCamps', async () => {
+		const { getByTestId } = render(
+			<CampSelectionContextProvider>
+				<TestComponent />
+			</CampSelectionContextProvider>
+		)
+
+		const addButton = getByTestId('addButton')
+		const removeButton = getByTestId('removeButton')
+		const campNumberP = getByTestId('campNumberElement')
+		const selectedCampsP = getByTestId('selectedCampsElement')
+
+		expect(campNumberP).toHaveTextContent('0')
+		expect(selectedCampsP).toHaveTextContent('0')
+
+		for (let i = 0; i < 100; i++) {
+			act(() => {
+				fireEvent.click(addButton)
+			})
+		}
+		expect(campNumberP).toHaveTextContent('100')
+		expect(selectedCampsP).toHaveTextContent('100')
+
+		act(() => {
+			fireEvent.click(removeButton)
+		})
+
+		expect(campNumberP).toHaveTextContent('0')
+		expect(selectedCampsP).toHaveTextContent('0')
+	})
+})
