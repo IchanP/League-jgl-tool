7660b0da43420afc202120382a55f14a52f1bdf1
Pontus Grandin <pg222pb@student.lnu.se>
2023-04-16 03:26:06 +0200

    Add cumulative levels in table

--- src/Components/ExpDisplay/ExpDisplay.jsx
+++ src/Components/ExpDisplay/ExpDisplay.jsx
@@ -4,13 +4,56 @@ import { CampSelectionContext } from '../../Contexts/CampSelectionContext'
 
 const ExpDisplay = () => {
 	const { selectedCamps, totalExp, level, totalRequired } = useContext(CampSelectionContext)
+	const [copiedStateArray, setCopiedStateArray] = useState([])
+	const expThresholds = [0, 280, 660, 1140, 1720, 2400, 3180, 4060, 5040, 6120, 7300, 8580, 9960, 11440, 13020, 14700, 16480, 18360]
+	
+	useEffect(() => {
+		if (selectedCamps.length > 0) {
+			// Set initial data-expvalue and data-level
+			let currentExp = 0
+			let currentLevel = 1
+			for (let i = 0; i < selectedCamps.length; i++) {
+				console.log(Number(selectedCamps[i].dataset.expvalue))
+				let expValue
+				i === 0
+					? expValue = Number(selectedCamps[i].dataset.expvalue) + 150
+					: expValue = Number(selectedCamps[i].dataset.expvalue)
+				currentExp += expValue
+				if (currentExp >= expThresholds[currentLevel]) {
+					currentLevel++
+				}
+				console.log(`currentExp: ${currentExp} and currentlevel ${currentLevel} and expthreshhold ${expThresholds[currentLevel]}`)
+				selectedCamps[i].setAttribute('data-level', currentLevel)
+			}
+			setCopiedStateArray(selectedCamps)
+		}
+	}, [selectedCamps])
+    
+    
+
 	return (  
 		<div className="expDisplay">
-			{selectedCamps.map((camp) => (
-				<p className="campValues" key={camp.id}>{camp.id} and expvalue: {camp.dataset.expvalue} and goldvalue: {camp.dataset.goldvalue}</p>
-			))}
+			<table>
+				<thead>
+					<tr>
+						<th>Camp ID</th>
+						<th>Level</th>
+						<th>Cumulative Gold</th>
+					</tr>
+				</thead>
+				<tbody>
+					{copiedStateArray.map((camp) => (
+						<tr key={camp.id}>
+							<td className="campName-td">{camp.id}</td>
+							<td>{camp.dataset.level}</td>
+							<td>{}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 			<p className="totalExp" data-testid="totalExp">Current: {totalExp}/{totalRequired[level -1]} Level: {level}</p>
 		</div>
+
 	)
 }
  
