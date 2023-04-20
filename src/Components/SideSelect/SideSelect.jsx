4bd3c33fe31c041c47abb3d28103ffb34f6e6bc1
Pontus Grandin <pg222pb@student.lnu.se>
2023-04-20 07:33:54 +0200

    Prevent unchecking of both checkboxes

--- src/Components/SideSelect/SideSelect.jsx
+++ src/Components/SideSelect/SideSelect.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './sideselect.css'
 
 /**
@@ -6,17 +6,62 @@ import './sideselect.css'
  * @returns {HTMLElement} - Returns a sideselector element
  */
 const SideSelect = () => {
-	return ( 
+	const [selected, setSelected] = useState(['bluebox', 'redbox'])
+  
+	/**
+	 * Verifies that the other checkbox is not unchecked before unchecking the checkbox.
+	 * @param {HTMLElement} target - The target element
+	 */
+	const sidesOnChange = (target) => {
+		const isChecked = target.checked
+		const id = target.id
+		const otherId = id === 'bluebox' ? 'redbox' : 'bluebox'
+		const otherCheckbox = document.getElementById(otherId)
+      
+		if (!isChecked) {
+			// if target checkbox is checked, verify if the other checkbox is also checked
+			if (!otherCheckbox.checked) {
+				// if the other checkbox is unchecked, prevent unchecking the target checkbox
+				target.checked = true
+				return
+			}
+			// if both checkboxes are checked, update the selected state with both ids
+			setSelected([id, otherId])
+		} else {
+			// if target checkbox is unchecked, update the selected state with the other id only
+			setSelected([otherId])
+		}
+	}
+      
+      
+      
+      
+  
+	return (
 		<>
 			<p className="selectsidep">Select side(s)</p>
-			<label className='sidebox-label' htmlFor="bluebox" id="bluelabel">
-				<input type="checkbox" className='sidebox' id="bluebox" defaultChecked/>
+			<label className="sidebox-label" htmlFor="bluebox" id="bluelabel">
+				<input
+					type="checkbox"
+					className="sidebox"
+					id="bluebox"
+					defaultChecked
+					onChange={(e) => sidesOnChange(e.target)}
+				/>
 			</label>
-			<label className='sidebox-label' htmlFor="redbox" id="redlabel">
-				<input type="checkbox" className='sidebox' id="redbox" defaultChecked/>
+			<label className="sidebox-label" htmlFor="redbox" id="redlabel">
+				<input
+					type="checkbox"
+					className="sidebox"
+					id="redbox"
+					defaultChecked
+					onChange={(e) => sidesOnChange(e.target)}
+				/>
 			</label>
 		</>
 	)
 }
+  
+
  
 export default SideSelect
